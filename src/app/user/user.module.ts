dd360b4728f11d29078e9ab0daa051238adf60e8
MGYOSBEL <mgyosbel@gmail.com>
2020-01-22 01:43:29 -0500

    Login - codeOrganization userModule organized
    User module is organized as follows:
    Dashboard Component
    - Shows info about a particular user.
    
    Models folder
    - Contains the user interface
    
    Services
    - User service for fetching user data.

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { UserComponent } from './user/user.component';
 import { UserRoutingModule } from './user-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { UserService } from './services/user.service';
 
 
 
 @NgModule({
   declarations: [
-    UserComponent,
      DashboardComponent
     ],
   imports: [
