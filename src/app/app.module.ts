c8d0c3b385b42d5d0cab9e424fe3a95c459f5b03
MGYOSBEL <mgyosbel@gmail.com>
2020-02-01 01:51:53 -0500

    User - Designing navbar devDashboardLayout
    Se diseno un navbar con 4 secciones:
    - Logo(col-3)
    - Search Bar (col-6)
    - User menu (col-2)
    - Menu (col-1)
    
    Por el momento todos los contenidos son estaticos.
    Puede haber detalles sujetos a cambios de diseno.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,9 @@ import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { InternalServerErrorComponent } from './errors/internal-server-error/internal-server-error.component';
 import { AuthInterceptor } from './auth/helpers/auth.interceptor';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FiltersComponent } from './components/filters/filters.component';
 
 const config = new AuthServiceConfig([
   {
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserRoutingModule } from './user-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavbarComponent } from '@app/components/navbar/navbar.component';
 
 
 
 @NgModule({
   declarations: [
-     DashboardComponent
+     DashboardComponent,
+     NavbarComponent
     ],
   imports: [
     CommonModule,
