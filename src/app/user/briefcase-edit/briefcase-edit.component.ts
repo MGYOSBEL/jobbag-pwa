d2bf15dae77c02d3ae7d6d5f3a0d82fad1e1413b
MGYOSBEL <mgyosbel@gmail.com>
2020-02-06 23:14:35 -0500

    user - BriefCase Edit Form was added
    Se agrego el briefcaseEditForm (es una casacara vacia en el template).
    Los datos en el .ts ya estan listos, solo faltarian los validators.
    Se deben agregar aun los componentes en el template(HTML) y enlazarlos a
    los formControl del componente.

--- src/app/user/briefcase-edit/briefcase-edit.component.ts
+++ src/app/user/briefcase-edit/briefcase-edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Briefcase } from '../models/user.model';
+import { Briefcase, Profession } from '../models/user.model';
 import { UserService } from '../services/user.service';
+import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-birefcase-edit',
@@ -10,12 +11,44 @@ import { UserService } from '../services/user.service';
 export class BriefcaseEditComponent implements OnInit {
 
   briefcases: Briefcase[];
+  professions: Profession[];
 
-  constructor(private userService: UserService) { }
+
+  briefcaseEditForm: FormGroup;
+  title = new FormControl('');
+  comments = new FormControl('');
+  description = new FormControl('');
+  startDate = new FormControl('');
+  endDate = new FormControl('');
+
+  constructor(private userService: UserService,
+              private formBuilder: FormBuilder) {
+    this.briefcaseEditForm = this.formBuilder.group({
+      title: [''],
+      comments: [''],
+      description: [''],
+      startDate: [''],
+      endDate: [''],
+      profession: ['']
+    });
+              }
 
   ngOnInit() {
-    const bc = this.userService.getBriefcase()
-    this.briefcases = bc ? bc : [] ;
+    this.briefcases = this.userService.getBriefcase();
+    this.userService.getAllProfessions().subscribe(
+      data => {
+        this.professions = data;
+      });
+  }
+
+  selectProfession(e) {
+    this.profession.setValue(e.target.value, {
+       onlySelf: true
+    });
+  }
+
+  get profession() {
+    return this.briefcaseEditForm.get('profession');
   }
 
   save() {}
