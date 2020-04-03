8856896309575b10df1d3f66a08eb5ec6852218a
MGYOSBEL <mgyosbel@gmail.com>
2020-04-03 00:50:54 -0400

    user - mediaService added
    Se agrego el servicio mediaService para las tareas de edicion y
    eliminacion de la foto de perfil y el CV. Extendible al resto de los
    archivos multimedia.

---
+++ src/app/user/services/media.service.ts
@@ -0,0 +1,118 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { APIResponse } from '@app/models/app.model';
+import { environment } from '@environments/environment';
+import { catchError, map, tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MediaService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+    editProfilePicture(userProfileId: number, picture: string): Observable<boolean> {
+      const request = {
+        client_id: environment.clientId,
+        client_secret: environment.clientSecret,
+        user_profile_id: userProfileId,
+        picture
+      };
+      return this.http.put<APIResponse>(`${environment.apiBaseURL}/media/userProfilePicture`, request).pipe(
+        catchError(err => {
+          throw  new Error(err.error.status + ': ' + err.error.detail);  // Relanzo el error con el status y el detail
+        }),
+        map( response => {
+          const content = JSON.parse(response.content); // Seleccionar la parte del response q es el contenido
+          if (response.status_code === 200) {
+            return content === 'OK'; // Retorno el content del response como cuerpo del observable
+          } else { // Si no fue OK el status del response lanzo un error con el status code y el text del response.
+            throw new Error(
+              response.status_code + ': ' + response.content.text
+            );
+          }
+        }),
+        tap() // Debo actualizar el profile, pues la url del profilePicture cambia
+      );
+    }
+
+    editProfileCV(userProfileId: number, cv: string): Observable<boolean> {
+      const request = {
+        client_id: environment.clientId,
+        client_secret: environment.clientSecret,
+        user_profile_id: userProfileId,
+        picture: cv
+      };
+      return this.http.put<APIResponse>(`${environment.apiBaseURL}/media/userProfileCV`, request).pipe(
+        catchError(err => {
+          throw  new Error(err.error.status + ': ' + err.error.detail);  // Relanzo el error con el status y el detail
+        }),
+        map( response => {
+          const content = JSON.parse(response.content); // Seleccionar la parte del response q es el contenido
+          if (response.status_code === 200) {
+            return content === 'OK'; // Retorno el content del response como cuerpo del observable
+          } else { // Si no fue OK el status del response lanzo un error con el status code y el text del response.
+            throw new Error(
+              response.status_code + ': ' + response.content.text
+            );
+          }
+        }),
+        tap() // Debo actualizar el profile, pues la url del profileCV cambia
+      );
+    }
+
+    deleteProfilePicture(userProfileId: number, picture: string): Observable<boolean> {
+      const request = {
+        client_id: environment.clientId,
+        client_secret: environment.clientSecret,
+        user_profile_id: userProfileId,
+        picture
+      };
+      return this.http.request<APIResponse>('DELETE', `${environment.apiBaseURL}/media/userProfilePicture`, { body: request}).pipe(
+        catchError(err => {
+          throw  new Error(err.error.status + ': ' + err.error.detail);  // Relanzo el error con el status y el detail
+        }),
+        map( response => {
+          const content = JSON.parse(response.content); // Seleccionar la parte del response q es el contenido
+          if (response.status_code === 200) {
+            return content === 'OK'; // Retorno el content del response como cuerpo del observable
+          } else { // Si no fue OK el status del response lanzo un error con el status code y el text del response.
+            throw new Error(
+              response.status_code + ': ' + response.content.text
+            );
+          }
+        }),
+        tap() // Debo actualizar el profile, pues la url del profilePicture cambia
+      );
+    }
+
+    deleteProfileCV(userProfileId: number, cv: string): Observable<boolean> {
+      const request = {
+        client_id: environment.clientId,
+        client_secret: environment.clientSecret,
+        user_profile_id: userProfileId,
+        picture: cv
+      };
+      return this.http.request<APIResponse>('DELETE', `${environment.apiBaseURL}/media/userProfileCV`, { body: request}).pipe(
+        catchError(err => {
+          throw  new Error(err.error.status + ': ' + err.error.detail);  // Relanzo el error con el status y el detail
+        }),
+        map( response => {
+          const content = JSON.parse(response.content); // Seleccionar la parte del response q es el contenido
+          if (response.status_code === 200) {
+            return content === 'OK'; // Retorno el content del response como cuerpo del observable
+          } else { // Si no fue OK el status del response lanzo un error con el status code y el text del response.
+            throw new Error(
+              response.status_code + ': ' + response.content.text
+            );
+          }
+        }),
+        tap() // Debo actualizar el profile, pues la url del profileCV cambia
+      );
+    }
+
+
+}
