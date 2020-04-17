61997272d7476416fd60c912d7288f55a0434696
MGYOSBEL <mgyosbel@gmail.com>
2020-04-16 23:06:29 -0400

    user - user Service refactored
    Se modifico userService para utilizar el servicio UserCacheService.
    Todos los accesos a la cache se realizan a traves de este servicio.
    
    Ademas se agrego userRole para saber si el usuario esta trabajando como
    CLIENT o SERVICE_PROVIDER.
    
    userRole es un atributo privado y se accede a traves de los
    setter/getter role.

--- src/app/user/services/user.service.ts
+++ src/app/user/services/user.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { User, UserProfile, UserProfileBriefcase } from '../models/user.model';
 import { LoggingService } from '@app/services/logging.service';
 import { environment } from '@environments/environment';
 import { map, tap, catchError } from 'rxjs/operators';
 import { APIResponse } from '@app/models/app.model';
+import { UserCacheService } from './user-cache.service';
 
 @Injectable({
   providedIn: 'root'
@@ -15,9 +16,15 @@ export class UserService {
   private _loggedUser: User;
   apiPath = environment.apiBaseURL;
 
+  private userRole: string; // Es para saber si el usuario esta autenticado como CLIENT o SERVICE_PROVIDER
 
-  constructor(private http: HttpClient,
-              private logging: LoggingService) { }
+
+  constructor(
+    private http: HttpClient,
+    private userCacheService: UserCacheService,
+    private logging: LoggingService) {
+      this.userRole = 'CLIENT';
+    }
 
 
   public get loggedUser(): User {
@@ -25,44 +32,34 @@ export class UserService {
   }
 
 
+  public get role(): string {
+    return this.userRole;
+  }
+
+  public set role(role: string) {
+    this.userRole = role;
+  }
+
+
   get(userId: string): Observable<User> {
     // const user_id = JSON.parse(localStorage.getItem('bearerToken')).user_id;
     return this.http.get<APIResponse>(this.apiPath + '/user/get/' + userId).pipe(
-      catchError(err => { // Captura si hubo algun error en la llamada y lo relanza
-        console.log(err);
-        throw new Error(err);  // Relanzo el error con el status y el detail
-      }),
       map(response => {
         if (response.status_code === 200) { // Si el status del response es OK retorno contento como dato del observable
           return JSON.parse(JSON.parse(response.content));
         } else {
-          throw new Error( // Si no es OK el status del response, lanzo un error con el status y el text
+          return throwError( // Si no es OK el status del response, lanzo un error con el status y el text
             response.status_code + ': ' + response.content.text
           );
-        }      }),
+        }
+      }),
+      catchError(err => { // Captura si hubo algun error en la llamada y lo relanza
+        console.log(err);
+        return throwError(err);  // Relanzo el error con el status y el detail
+      }),
       tap((response: User) => {
         this._loggedUser = response; // Salvo el user en el storage
-        localStorage.setItem('loggedUser', JSON.stringify({
-          id: response.id,
-          username: response.username,
-          email: response.email
-        }));
-        if (response.profiles.length) {
-          const userProfiles: Array<UserProfile> = response.profiles; // Salvo los userProfiles en el Storage
-          localStorage.setItem('userProfiles', JSON.stringify(response.profiles));
-          console.log(userProfiles);
-          const index = userProfiles.findIndex(elem => elem.userProfileType === 'SERVICE_PROVIDER');
-          console.log('index', index);
-          if (index >= 0) {
-            // Salvo los briefcases en el Storage
-            const briefcases: Array<UserProfileBriefcase> = userProfiles[index].briefcases || [];
-            localStorage.setItem('briefcases', JSON.stringify(briefcases));
-
-          }
-        } else { // No hay ningun perfil creado, guardo los arrays vacios
-          localStorage.setItem('userProfiles', JSON.stringify([]));
-          localStorage.setItem('briefcases', JSON.stringify([]));
-        }
+        this.userCacheService.setUser(response);
       })
     );
   }
@@ -73,30 +70,25 @@ export class UserService {
       client_secret: environment.clientSecret,
       user: data
     };
-    console.log('data request: ' ,  req);
+    console.log('data request: ', req);
     return this.http.put<APIResponse>(this.apiPath + '/user', req).pipe(
       map(response => {
         const content = JSON.parse(response.content);
         console.log(content);
         if (response.status_code === 200) {
-          localStorage.setItem('User', JSON.stringify(req.user));
           return content;
         } else {
-          return {
-            error: true,
-            statusCode: response.status_code,
-            text: response.content.text
-          };
+          return throwError( // Si no es OK el status del response, lanzo un error con el status y el text
+            response.status_code + ': ' + response.content.text
+          );
         }
-      }, err => {
-        console.log('SERVER ERROR!!!!!');
-        console.log(err);
       }),
-      tap(response => {
-        console.log('data response ' , response);
-        if (!response.error) {
-          localStorage.setItem('loggedUser', (response));
-        }
+      catchError(err => { // Captura si hubo algun error en la llamada y lo relanza
+        console.log(err);
+        return throwError(err);  // Relanzo el error con el status y el detail
+      }),
+      tap((response: User) => {
+        this.userCacheService.setUser(response);
       })
     );
   }
