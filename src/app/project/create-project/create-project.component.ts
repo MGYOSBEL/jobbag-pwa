1baedd4ec7c0ba9236e3ba98757af66a70a61835
victor198siete <victor198siete@gmail.com>
2020-06-02 12:09:59 -0400

    desCreateProjectForm

--- src/app/project/create-project/create-project.component.ts
+++ src/app/project/create-project/create-project.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-create-project',
@@ -7,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateProjectComponent implements OnInit {
 
-  constructor() { }
+  createProjectForm = new FormGroup({});
+
+
+
+  constructor(private formBuilder: FormBuilder) {
+    this.createProjectForm = this.formBuilder.group({
+    });
+  }
 
   ngOnInit() {
   }
