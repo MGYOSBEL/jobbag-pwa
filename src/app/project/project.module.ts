436fe725252413e23893f30fa24c7a25d6f33e5b
MGYOSBEL <mgyosbel@gmail.com>
2020-06-02 22:02:52 -0400

    solved refresh issue

--- src/app/project/project.module.ts
+++ src/app/project/project.module.ts
@@ -5,12 +5,16 @@ import { ProjectRoutingModule } from './project-routing.module';
 import { EditProjectComponent } from './edit-project/edit-project.component';
 import { CreateProjectComponent } from './create-project/create-project.component';
 import { ProjectDetailComponent } from './project-detail/project-detail.component';
+import { SharedComponentsModule } from '@app/sharedComponents/shared-components.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [EditProjectComponent, CreateProjectComponent, ProjectDetailComponent],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
+    SharedComponentsModule,
     ProjectRoutingModule
   ]
 })
