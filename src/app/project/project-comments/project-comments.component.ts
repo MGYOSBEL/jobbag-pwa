1a887930523674c094cd56a8f03a2e6a6b0ec2e1
victor198siete <victor198siete@gmail.com>
2020-06-27 10:42:48 -0400

    dev-desProjectDetail commit

---
+++ src/app/project/project-comments/project-comments.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-project-comments',
+  templateUrl: './project-comments.component.html',
+  styleUrls: ['./project-comments.component.css']
+})
+export class ProjectCommentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
