c3bed23e5cc56bbb39e7663e4547a46ad885ff6a
MGYOSBEL <mgyosbel@gmail.com>
2020-07-14 08:43:33 -0400

    location filter done

--- src/app/project/models/filters.ts
+++ src/app/project/models/filters.ts
@@ -2,13 +2,15 @@ import { Project } from './project.model';
 import { DivisionElement } from '@app/user/models/country.model';
 
 export function filterByLocation(projects: Project[], locationFilters: number[]): Project[] {
-  const filterProjects = projects.filter(project => project.divisions.some(division => locationFilters.includes(division)));
+  console.log('projects before filtering => ', projects);
+  const filterProjects = projects.filter(
+    project => project.remote || project.divisions.filter(division => locationFilters.includes(division)).length > 0);
   console.log('filtered by Location => ', filterProjects);
   return filterProjects;
 }
 
 export function filterByService(projects: Project[], serviceFilters: number[]): Project[] {
   const filterProjects = projects.filter(project => project.services.some(service => serviceFilters.includes(service)));
-  console.log('filtered by Location => ', filterProjects);
+  console.log('filtered by services => ', filterProjects);
   return filterProjects;
 }
