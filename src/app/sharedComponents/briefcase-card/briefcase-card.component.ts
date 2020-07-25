554699d6f613ff8081b16b35d53095ee735d3276
MGYOSBEL <mgyosbel@gmail.com>
2020-07-25 18:23:19 -0400

    briefcase added to userProfile

--- src/app/sharedComponents/briefcase-card/briefcase-card.component.ts
+++ src/app/sharedComponents/briefcase-card/briefcase-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-briefcase-card',
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BriefcaseCardComponent implements OnInit {
 
+  @Input()
+  params: {
+    id: number;
+    verified: boolean;
+    picture: string;
+  };
+
+  @Output()
+  viewDetail = new EventEmitter<number>();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onBriefcaseDetail() {
+    this.viewDetail.emit(this.params.id);
   }
 
 }
